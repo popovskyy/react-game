caec2277e5bd9b41064d4974a7047a1a9ed8f366
popovskiy <roman.p@overdose.digital>
2024-02-02 10:40:18 +0200

    Build color mode switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar.tsx";
+
 function App() {
-  return <div></div>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
+      </Show>
+
+      <GridItem area="main">Main</GridItem>
+    </Grid>
+  );
 }
 
 export default App;
